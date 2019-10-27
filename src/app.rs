949d8370c3770dbd22437df699efd857885d0ce7
Kitsu <mail@kitsu.me>
2019-10-28 01:20:18 +0300

    fix(app): use ctrl+p hotkey for pause

--- src/app.rs
+++ src/app.rs
@@ -202,7 +202,7 @@ impl App {
                 Key::Right => player_commands.send(Command::NextTrack)?,
                 Key::Left => player_commands.send(Command::PrevTrack)?,
                 Key::Delete => return Ok(()),
-                Key::Char(' ') => player_commands.send(Command::Pause)?,
+                Key::Ctrl('p') => player_commands.send(Command::Pause)?,
                 Key::Char(']') => player_commands.send(Command::Forward5)?,
                 Key::Char('[') => player_commands.send(Command::Backward5)?,
                 Key::Ctrl('r') => drawer.draw()?,
